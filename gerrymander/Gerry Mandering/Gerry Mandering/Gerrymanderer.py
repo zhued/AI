9afce4214f193b857fe81d5db10ad5ab8a23756c
Edward Zhu <zhued>
2014-10-21 20:25:35 -0600

    removed bogus

--- gerrymander/Gerry Mandering/Gerry Mandering/Gerrymanderer.py
+++
@@ -1,8 +0,0 @@
-class Gerrymanderer:
-	neighborhood = None;
-	def __init__(self,neighborhood):
-		self.neighborhood = neighborhood
-
-	#Not yet created
-	def setDistrictsForParty(self,maxParty,minParty):
-		return None
