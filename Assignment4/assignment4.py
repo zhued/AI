d575d3f6561c82bc979101846cc711f29a055052
Edward Zhu <edward.r.zhu@gmail.com>
2014-11-08 10:06:25 -0700

    added some things

--- Assignment4/assignment4.py
+++ Assignment4/assignment4.py
@@ -1,4 +1,24 @@
 # Edward Zhu
 # Artificial Intellengence - CSCI 3202
-# Collaborated with:
+# Collaborated with: Josh Fermin, Louis, Sheefali Tewari, Andrew Arnip
 #	
+
+import getopt
+import sys
+
+def main():
+	options, remainder = getopt.getopt(sys.argv[1:], 'gjm')
+	for opt, arg in options:
+		if opt in ('-g'):
+			something = 'g'
+		elif opt in ('-j'):
+			something = 'j'
+		elif opt in ('-m'):
+			something = 'm'
+
+
+	print 'remainder =', remainder
+	print 'something =', something
+
+if __name__ == "__main__":
+	main()
