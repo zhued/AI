3ee3cd3bce4946dea1cfddff0e57741a756821f6
Edward Zhu <edward.r.zhu@gmail.com>
2014-11-11 15:52:43 -0700

    conditional and marginal kinda working

---
+++ Assignment4/CancerModel.py
@@ -0,0 +1,86 @@
+import sys
+from numpy import *
+from pbnt.Graph import *
+from pbnt.Distribution import *
+from pbnt.Node import *
+
+
+def cancer():
+    #testing basic bayes net class implementation
+    numberOfNodes = 5
+    #name the nodes
+    pollution = 0
+    smoker = 1
+    cancer = 2
+    xray = 3
+    dyspnoea = 4
+
+    pNode = BayesNode(0, 2, name="pollution")
+    sNode = BayesNode(1, 2, name="smoker")
+    cNode = BayesNode(2, 2, name="cancer")
+    xNode = BayesNode(3, 2, name="xray")
+    dNode = BayesNode(4, 2, name="dyspnoea")
+
+    #Pollution
+    pNode.add_child(cNode)
+
+    #smoker
+    sNode.add_child(cNode)
+
+    #cancer
+    cNode.add_parent(pNode)
+    cNode.add_parent(sNode)
+    cNode.add_child(xNode)
+    cNode.add_child(dNode)
+
+    #xray
+    xNode.add_parent(cNode)
+
+    #dyspnoea
+    dNode.add_parent(cNode)
+
+    nodes = [pNode, sNode, cNode, xNode, dNode]
+
+    #create distributions
+    #pollution
+    pDistribution = DiscreteDistribution(pNode)
+    index = pDistribution.generate_index([],[])
+    # high low
+    pDistribution[index] = [0.10, 0.90]
+    pNode.set_dist(pDistribution)
+
+    #smoker
+    sDistribution = DiscreteDistribution(sNode)
+    index = sDistribution.generate_index([],[])
+    # false true
+    sDistribution[index] = [0.70, 0.30]
+    sNode.set_dist(sDistribution)
+
+    #cancer
+    dist = zeros([pNode.size(), sNode.size(), cNode.size()], dtype=float32)
+    # P = High -> 0, S = False -> 0
+    dist[0,0,] = [0.98,0.02]
+    dist[1,0,] = [0.999,0.001]
+    dist[0,1,] = [0.95,0.05]
+    dist[1,1,] = [0.97,0.03]
+    cDistribution = ConditionalDiscreteDistribution(nodes=[pNode, sNode, cNode], table=dist)
+    cNode.set_dist(cDistribution)
+
+    #xray
+    dist = zeros([cNode.size(), xNode.size()], dtype=float32)
+    dist[0,] = [0.1,0.9]
+    dist[1,] = [0.8,0.2]
+    xDistribution = ConditionalDiscreteDistribution(nodes=[cNode, xNode], table=dist)
+    xNode.set_dist(xDistribution)
+
+    #dyspnoea
+    dist = zeros([cNode.size(), dNode.size()], dtype=float32)
+    dist[0,] = [0.35,0.65]
+    dist[1,] = [0.7,0.3]
+    dDistribution = ConditionalDiscreteDistribution(nodes=[cNode, dNode], table=dist)
+    dNode.set_dist(dDistribution)
+
+
+    #create bayes net
+    bnet = BayesNet(nodes)
+    return bnet
--- Assignment4/assignment4.py
+++ Assignment4/assignment4.py
@@ -1,24 +1,192 @@
 # Edward Zhu
 # Artificial Intellengence - CSCI 3202
-# Collaborated with: Josh Fermin, Louis, Sheefali Tewari, Andrew Arnip
+# Collaborated with: Josh Fermin, Louis, Sheefali Tewari
 #	
 
-import getopt
-import sys
+
+# P=Pollution
+# S=Smoker
+# C=Cancer
+# D=Dyspnoea
+# X=Xray
+ 
+import getopt, sys, re
+from numpy import *
+import CancerModel as Cancer
+from pbnt.Graph import *
+from pbnt.Distribution import *
+from pbnt.Node import *
+from pbnt.Inference import *
+
+
+def conditional_prob(args, BayesNet, engine):
+	conditionalTuples = []
+
+	left, given = parseConditionalArgs(args)
+
+	leftTuple = bruteforcetuple(BayesNet, left)
+
+	#list of tuples for given conditions
+	for letter in given:
+		conditionalTuples.append(bruteforcetuple(BayesNet, letter))
+
+	for condition, truth in conditionalTuples:
+		engine.evidence[condition] = truth
+
+	# left side of | 
+	toCalculate, leftTruth = leftTuple
+
+	Q = engine.marginal(toCalculate)[0]
+	index = Q.generate_index([leftTruth], range(Q.nDims))
+	conditionalProbablity = Q[index]
+
+	print "The conditional probability of", toCalculate.name, "=", leftTruth, ", given", given, "is: ", conditionalProbablity
+	return conditionalProbablity
+
+def joint_prob(args, BayesNet):
+	print "\nThe joint probability for " + args + " is:\n"
+	argtype = checkArgs(args)
+	arglookup = findArgValue(args)
+	for node in BayesNet.nodes:
+		if node.value == arglookup:
+			if argtype == "lower":
+				return
+			if argtype == "upper":
+				return
+			if argtype == "tilda":
+				return
+
+def marginal_prob(args, BayesNet, engine):
+	arglookup = findArgValue(args)
+	if len(args) > 1:
+		print "Marginal Probability Distribution can only take one argument"
+		sys.exit(2)
+	for node in BayesNet.nodes:
+		if node.id == 0 and arglookup == 'p':
+			CalcNode = node
+		if node.id == 1 and arglookup == 's':
+			CalcNode = node
+		if node.id == 2 and arglookup == 'c':
+			CalcNode = node
+		if node.id == 3 and arglookup == 'x':
+			CalcNode = node
+		if node.id == 4 and arglookup == 'd':
+			CalcNode = node
+
+	Q = engine.marginal(CalcNode)[0]
+	argtype = checkArgs(args)
+	if argtype == "lower":
+		index = Q.generate_index([True], range(Q.nDims))
+		print "\nThe marginal probability of " + CalcNode.name + "=true: ", Q[index]
+		return Q[index]
+	elif argtype == "tilda":
+		index = Q.generate_index([False], range(Q.nDims))
+		print "\nThe marginal probability of " + CalcNode.name + "=false: ", Q[index]
+		return Q[index]
+	elif argtype == "upper":
+		index = Q.generate_index([True], range(Q.nDims))
+		print "\nThe marginal probability of " + CalcNode.name + "=true: ", Q[index]
+		index = Q.generate_index([False], range(Q.nDims))
+		print "The marginal probability of " + CalcNode.name + "=false: ", Q[index]
+
+
 
 def main():
-	options, remainder = getopt.getopt(sys.argv[1:], 'gjm')
+	BayesNet = Cancer.cancer()
+	engine = JunctionTreeEngine(BayesNet)
+
+	options, remainder = getopt.getopt(sys.argv[1:], 'g:j:m:')
 	for opt, arg in options:
 		if opt in ('-g'):
-			something = 'g'
+			conditional_prob(arg, BayesNet, engine)
+
 		elif opt in ('-j'):
-			something = 'j'
-		elif opt in ('-m'):
-			something = 'm'
+			joint_prob(arg, BayesNet)
+			# find conditional prob, then marginal, then multiply together
+
+		elif opt in ('-m'):			
+			marginal_prob(arg, BayesNet, engine)
+		else:
+			assert False, "unhandled option"
+
+def bruteforcetuple(BayesNet, letter):
+	for node in BayesNet.nodes:
+		if node.id == 0:
+			pollution = node
+		if node.id == 1:
+			smoker = node
+		if node.id == 2:
+			cancer = node
+		if node.id == 3:
+			xray = node
+		if node.id == 4:
+			dyspnoea = node
+
+	if letter == 'p':
+		returntuple = (pollution, True)
+	elif letter == 's':
+		returntuple = (smoker, True)
+	elif letter == 'c':
+		returntuple = (cancer, True)
+	elif letter == 'x':
+		returntuple = (xray, True)
+	elif letter == 'd':
+		returntuple = (dyspnoea, True)
+	elif letter == '~p':
+		returntuple = (pollution, False)
+	elif letter == '~s':
+		returntuple = (smoker, False)
+	elif letter == '~c':
+		returntuple = (cancer, False)
+	elif letter == '~x':
+		returntuple = (xray, False)
+	elif letter == '~d':
+		returntuple = (dyspnoea, False)
+	else:
+		print "Please give a good condition."
+		exit()
+
+	return returntuple
 
 
-	print 'remainder =', remainder
-	print 'something =', something
+def parseConditionalArgs(args):
+	splitPipe = args.split('|')
+	left = splitPipe[0]
+	right = splitPipe[1]
+
+	given = list(right)
+	given = iter(given)
+	conditionalArgs2 = []
+	skip = False
+	for string in given:
+		if string == "~":
+			conditionalArgs2.append("~" + given.next())
+			continue
+		else:
+			conditionalArgs2.append(string)
+	query = left
+	return query, conditionalArgs2
+
+
+# check if false/true or a distribution
+def checkArgs(args):
+	if args.islower():
+		if "~" in args:
+			return "tilda"
+		else:
+			return "lower"
+	elif args.isupper():
+		return "upper"
+
+# returns the letter in lowercasepyc
+def findArgValue(args):
+	if args.islower():
+		if "~" in args:
+			return args.translate(None, '~')
+		else:
+			return args
+	elif args.isupper():
+		return args.lower()
 
 if __name__ == "__main__":
 	main()
